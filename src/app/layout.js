418498051efe98263e6bff4c5689a0efa0a13f4d
Lees20 <contact@pkarabetsos.com>
2025-03-12 14:46:30 +0200

    Fixed issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,8 @@ import { Inter, Domine } from "next/font/google";
 import "./globals.css";
 import { Playfair_Display } from "next/font/google";
 import { Montserrat } from "next/font/google";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const domine = Domine({ subsets: ["latin"], variable: "--font-domine" });
