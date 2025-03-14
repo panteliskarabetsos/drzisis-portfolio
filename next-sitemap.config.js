397b36b634e63a7f77f6f1a24269c3e49f6b4d0e
Lees20 <contact@pkarabetsos.com>
2025-03-14 14:12:06 +0200

    created sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://yourwebsite.com', // Replace with your actual site URL
+    generateRobotsTxt: true, // Generate robots.txt
+  };
+
+  // sitemap.xml
