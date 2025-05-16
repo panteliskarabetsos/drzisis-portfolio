b8fef769ebb48f5ba6ed82de0dae25cf09cec59c
Pantelis Karabetsos <panteliskarabetsos@Mac.home>
2025-05-16 19:54:55 +0300

    Added page not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center px-6 py-24 bg-white text-center text-gray-800">
+    <main className="min-h-screen flex flex-col items-center justify-center px-6 py-24 bg-white text-center text-gray-800 user-select-none">
       <div className="max-w-xl">
         
         <img
